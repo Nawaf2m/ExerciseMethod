068cb9c1dcbbfacd0e847497328bb6902846d830
Nawaf <nawaf.o_o@hotmail.com>
2026-08-18 15:50:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         //Q1
-//        int smallest = findSmallestNumber();
-//        System.out.println("The smallest value is " + smallest);
-//        System.out.println();
+        int smallest = findSmallestNumber();
+        System.out.println("The smallest value is " + smallest);
+        System.out.println();
         //Q2
-//        System.out.println("The number is " + checkNumber());
-//        System.out.println();
+        System.out.println("The number is " + checkNumber());
+        System.out.println();
         //Q3
         checkPassword();
     }
